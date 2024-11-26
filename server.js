75f77f0513dca137ce64b326b0b89857229be123
rinaldovilela <rinaldoalvesvilela@hotmail.com>
2024-11-26 12:57:11 -0300

    remove verificar garantias

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const mongoose = require("mongoose");
 const app = require("./src/app");
 const { verificarGarantias } = require("./src/utils/notificationScheduler");
 
+verificarGarantias();
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Conectado ao MongoDB"))
   .catch((err) => console.error(err));
-
-verificarGarantias();
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor rodando na porta ${process.env.PORT}`);
